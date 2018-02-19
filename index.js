5098fc0e84ff255b52f032f5de3a95430907ee2c
Manish Kumar <manish.kumar@networkbulls.in>
2018-02-20 02:08:55 +0530

    trying to add error handlers

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class ScheduleJobs{
 	*/
 	get userData(){
 
-		var user = {
+		let user = {
 		  uid: this.faker.random.uuid(),
 		  name: this.faker.name.findName(),
 		  email: this.faker.internet.email(),
@@ -22,7 +22,7 @@ class ScheduleJobs{
 	}
 	get employeeData(){
 
-		var employee = {
+		let employee = {
 		  uid: this.faker.random.uuid(),
 		  name: this.faker.name.findName(),
 		  salary: this.faker.finance.amount(),
@@ -33,7 +33,7 @@ class ScheduleJobs{
 	}
 	get studentData(){
 
-		var student = {
+		let student = {
 		  uid: this.faker.random.uuid(),
 		  name: this.faker.name.findName(),
 		  email: this.faker.internet.email(),
@@ -47,7 +47,7 @@ class ScheduleJobs{
 	// create a server to host index.html file
 	createServer(){
 		// load required modules, http =>server, ejs =>to combine data and template to generate html, take a reference to this for use in callback functions
-		var http = require('http'),ejs=require('ejs'),self = this;
+		let http = require('http'),ejs=require('ejs'),self = this;
 
 		console.log("creating an http server ....")
 		// return a new instance of http.Server.
@@ -69,29 +69,10 @@ class ScheduleJobs{
 		});
 	}
 
-	// initialize HTTP server and initializing socket.io to listen at this server
-	initServer(){
-		this.server = this.createServer();
-		this.socketio = require('socket.io').listen(this.server);
-	}
 
-	async fetchDocuments(collectionName){
-		console.log("fetching documents from "+collectionName+" collection");
-		return new Promise((resolve, reject)=>{
-			this.dbConnect(function(db){
-				var assert = require('assert');
-				db.collection(collectionName).find().sort({"_id":-1}).toArray(function(err,items){
-					resolve(items);
-				});
-			});
-
-		});
-
-	}
-
-
+	// connect to mongodb
 	dbConnect(callback){
-		var assert = require('assert');
+		let assert = require('assert');
 		this.MongoClient = require('mongodb').MongoClient;
 		this.uri = 'mongodb://localhost:27017';
 		this.MongoClient.connect(this.uri, function(err, client) {
@@ -103,24 +84,27 @@ class ScheduleJobs{
 
 	}
 	
-	emitData(db,collectionName,result,socketio){
-		if(result["insertedCount"]>0){
-			console.log("emitting "+collectionName+" message using socketio ....");
-			socketio.sockets.volatile.emit(collectionName, result["ops"][0]);
-		}
-		else{
-			console.log("no data inserted");			
-		}
+	// fetch documents from given collection in mongodb
+	async fetchDocuments(collectionName){
+		console.log("fetching documents from "+collectionName+" collection");
+		return new Promise((resolve, reject)=>{
+			this.dbConnect(function(db){
+				db.collection(collectionName).find().sort({"_id":-1}).toArray(function(err,items){
+					resolve(items);
+				});
+			});
 
-		console.log("close db connection after emitting message");
-		// db.close(); //not working throwing error as no close method for db / need to check
+		});
+
 	}
 
+
+	// insert single document to given collection and emit inserted record using socket.io 
 	insertDocuments(collectionName,singleDocument,socketio,callback){
 		console.log("connecting to db ...");
 		this.dbConnect(function(db){
 			console.log("successfully connected to mongodb");
-			var collection = db.collection(collectionName);
+			let collection = db.collection(collectionName);
 			collection.insert(singleDocument,{w:1},function(err, result) {
 			    console.log("-- Inserted 1 document into "+collectionName+" collection --");
 		  		callback(db,collectionName,result,socketio);
@@ -128,29 +112,46 @@ class ScheduleJobs{
 		});
 	}
 
+	// emit inserted record using socket.io to listening sockets as in client side 
+	emitData(db,collectionName,result,socketio){
+		if(result["insertedCount"]>0){
+			console.log("emitting "+collectionName+" message using socketio ....");
+			socketio.sockets.emit(collectionName, result["ops"][0]);
+		}
+		else{
+			console.log("no data inserted");			
+		}
+
+		// console.log("close db connection after emitting message");
+		// db.close(); //not working throwing error as no close method for db / need to check
+	}
+
+	// initialize HTTP server and initializing socket.io to listen at this server
+	initServer(){
+		this.server = this.createServer();
+		this.socketio = require('socket.io').listen(this.server);
+	}
+
 }
 
-var jobs=new ScheduleJobs();
+let jobs = new ScheduleJobs();
 jobs.initServer();
 
-var cron = require('node-cron');
+let cron = require('node-cron');
 
-cron.schedule('*/1 * * * *', function(){
-  	console.log('running a task every 1 minute');
-  	// console.log('running a task every minute at 13');
+cron.schedule('*/13 * * * * *', function(){
+  	console.log('running a task every 13 seconds');
 	jobs.insertDocuments("users",jobs.userData,jobs.socketio,jobs.emitData);
 
 });
 
-cron.schedule('*/2 * * * *', function(){
-  	console.log('running a task every 2 minutes');
-  	// console.log('running a task every minute at 37');
+cron.schedule('*/37 * * * * *', function(){
+  	console.log('running a task every 37 seconds');
 	jobs.insertDocuments("employees",jobs.employeeData,jobs.socketio,jobs.emitData);
 });
 
-cron.schedule('*/3 * * * *', function(){
-  	console.log('running a task every 3 minutes');
-  	// console.log('running a task every minute at 59');
+cron.schedule('*/59 * * * * *', function(){
+  	console.log('running a task every 59 seconds');
 	jobs.insertDocuments("students",jobs.studentData,jobs.socketio,jobs.emitData);
 });
 
